b345f301b49e2c77a66661d21387873efe870905
Jhorman Mera <1010159945@u.icesi.edu.co>
2023-05-09 16:14:22 -0500

    html and js linked

---
+++ frontend/js/index.js
@@ -0,0 +1,9 @@
+const console = document.getElementById('lblConsole');
+const price = document.getElementById('lblPrice');
+
+
+calculatePrice = ()=>{
+   window.alert('Calculating price...')    
+}
+
+btnCalculatePrice.addEventListener('click' , calculatePrice)
